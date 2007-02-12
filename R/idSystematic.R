adb1479a7c19ee4eccdb87ac8ac2b227b399dc1e
Tony Chiang <tchiang@fhcrc.org>
2007-02-12 20:04:20 +0000

    I have fixed alot of bugs and edited man pages to almost pass check
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@22594 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/idSystematic.R
+++ R/idSystematic.R
@@ -1,5 +1,5 @@
 idSystematic <- function(bpMat, viable, bpGraph=FALSE, pThresh=0.01,
-                         pLevels = c(1e-06, 1e-04, 0.01)){
+                         pLevels = 1e-4, prob=0.5){
 
     if(bpGraph)
       bpMat <- as(bpMat, "matrix")
