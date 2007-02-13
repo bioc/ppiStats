a387212a636dc8f2a5f2686ad2818b88504867bd
Tony Chiang <tchiang@fhcrc.org>
2007-02-13 15:03:43 +0000

    I have updated the supp.Rnw as per WH suggestions. I have also added two scripts that conducts the over/under representation analsis
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@22616 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/assessSymmetry.R
+++ R/assessSymmetry.R
@@ -17,7 +17,8 @@ assessSymmetry = function(bpMat, bpGraph = FALSE,
     p2 <- 1 - pbinom(n2, size=nunrec, prob=prob)
     p = pmin(p1+p2, 1) 
     nmax = 2*max(deg[, "no"], deg[, "ni"])
-    contours = sapply(pLevels, qbinom, size=0:nmax, prob=0.5)
+    contours = sapply(pLevels, qbinom, size=0:nmax, prob=prob)
+
     list(deg=deg, p=p, contours=contours)
     
 }
---
+++ inst/Scripts/hgPfam.R
@@ -0,0 +1,92 @@
+library(GO)
+library(Category)
+library(ppiStats)
+library(ppiData)
+library("YEAST")
+library("GOstats")
+
+
+yeastGenome <- names(as.list(YEASTALIAS))
+
+  ##------------------------------------------------------------------------------------------
+  cat("\n\nThis first code chunk will calculate the hypergeometric tests on the PFam Domains
+      to determine over-representation wrt viable baits/prey agains the genome. The resulting
+      data is saved in the R objects overVBGen and overVPGen", sep="" )
+  ##------------------------------------------------------------------------------------------
+
+parameterVB <- lapply(viableBaits, function(x,y){new("PFAMHyperGParams",
+                                      geneIds=x, 
+                                      universeGeneIds=yeastGenome,
+                                      annotation="YEAST", 
+                                      testDirection="over",
+                                      pvalueCutoff=0.01)})
+
+
+hgTestVB <- lapply(parameterVB, hyperGTest)
+overVBGen <- lapply(hgTestVB, function(x) {names(which(pvalues(x)<0.01))})
+
+
+
+parameterVP <- lapply(viablePrey, function(x,y){new("PFAMHyperGParams",
+                                      geneIds=x, 
+                                      universeGeneIds=yeastGenome,
+                                      annotation="YEAST", 
+                                      testDirection="over",
+                                      pvalueCutoff=0.01)})
+
+
+hgTestVP <- lapply(parameterVP, hyperGTest)
+overVPGen <- lapply(hgTestVP, function(x) {names(which(pvalues(x)<0.01))})
+
+  ##------------------------------------------------------------------------------------------
+  cat("\n\nNow we will set up the various parameters to conduct the hypergeometric tests
+      for biased proteins against the viable bait/prey populations", sep="")
+  ##------------------------------------------------------------------------------------------
+
+
+bpGraphs <- lapply(bpExperimentNames, get) ## could use 'mget'
+vbp <- mapply(function(x,y){intersect(x,y)},viableBaits,viablePrey)
+vbpGraph <- mapply(function(x,y){subGraph(x, get(y))},vbp,bpExperimentNames)
+
+vbpStochastic <- lapply(vbpGraph, function(x) {if(length(nodes(x))!=0) idStochastic(x, bpGraph=TRUE)})
+vbpSystematic <- mapply(function(x,y){setdiff(nodes(x),y)},vbpGraph,vbpStochastic)
+
+vbpSysNonTriv <- which(sapply(vbpSystematic,length) != 0)
+vbpSys <- vbpSystematic[vbpSysNonTriv]
+vbp1 <- vbp[vbpSysNonTriv]
+
+viableBaits <- viableBaits[vbpSysNonTriv]
+viablePrey <- viablePrey[vbpSysNonTriv]
+
+
+
+  ##------------------------------------------------------------------------------------------
+  cat("\n\nThis last code chunk will calculate the hypergeometric tests on the PFam Domains
+      to determine over-representation wrt biased proteins agains the VB and VP. The resulting
+      data is saved in the R objects overSysVB and overSysVP", sep="" )
+  ##------------------------------------------------------------------------------------------
+
+
+
+parameterSB <- mapply(function(x,y){new("PFAMHyperGParams",
+                                      geneIds=x, 
+                                      universeGeneIds=y,
+                                      annotation="YEAST", 
+                                      testDirection="over",
+                                      pvalueCutoff=0.01)},
+              vbpSys,viableBaits)
+
+hgTestSB <- lapply(parameterSB, hyperGTest)
+overSysVB <- lapply(hgTestSB, function(x) {names(which(pvalues(x)<0.01))})
+
+
+parameterSP <- mapply(function(x,y){new("PFAMHyperGParams",
+                                      geneIds=x, 
+                                      universeGeneIds=y,
+                                      annotation="YEAST", 
+                                      testDirection="over",
+                                      pvalueCutoff=0.01)},
+              vbpSys,viablePrey)
+hgTestSP <- lapply(parameterSP, hyperGTest)
+overSysVP <- lapply(hgTestSP, function(x) {names(which(pvalues(x)<0.01))})
+
