cf5c887cd4e57e8ad1b8e38f2c598cbf69b31ef3
Tony Chiang <tchiang@fhcrc.org>
2007-06-30 22:17:12 +0000

    I have updated the function idViableProteins to account for homomers
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@25601 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/idViableProteins.R
+++ R/idViableProteins.R
@@ -1,10 +1,23 @@
-idViableProteins <- function(bpGraph){
+idViableProteins <- function(bpGraph, homomer=TRUE){
 
-    deg <- degree(bpGraph)
-    vb <- names(which(deg$outDegree > 0))
-    vp <- names(which(deg$inDegree > 0))
-    vProt <- list()
-    vProt$viableBaits <- vb
-    vProt$viablePrey <- vp
-    vProt
+  bpMat <- as(bpGraph, "matrix")
+  if (!homomer){
+    diag(bpMat) <- 0
+  }
+    #deg <- degree(bpGraph)
+    #vb <- names(which(deg$outDegree > 0))
+    #vp <- names(which(deg$inDegree > 0))
+    
+  vb <- names(which(rowSums(bpMat)>0))
+  vp <- names(which(colSums(bpMat)>0))
+  vbp <- names(which(rowSums(bpMat)>0 & colSums(bpMat)>0))
+  vbp1 <- intersect(vb, vp)
+  if(length(setdiff(vbp, vbp1))>0 || length(setdiff(vbp1, vbp))){
+    stop("There is bug")
+  }
+  vProt <- list()
+  vProt$VB <- vb
+  vProt$VP <- vp
+  vProt$VBP <- vbp
+  vProt
 }
