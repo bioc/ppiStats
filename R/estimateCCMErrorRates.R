4bbe6226a6f52ffedf6b88c3fa26a41b978c3dae
Denise Scholtens <dscholtens@northwestern.edu>
2007-11-28 20:53:47 +0000

    updates to estimateCCMErrorRates.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@28953 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/estimateCCMErrorRates.R
+++ R/estimateCCMErrorRates.R
@@ -1,204 +1,167 @@
+
+
 estimateCCMErrorRates <- function(m,GS,filterSystematic=TRUE,
-                                  obsPropThresh=.7,SystematicpThresh=.01,
-                                  nBS = 100, seed = 123){
-  set.seed(123)
+                                  obsPropThresh=1,SystematicpThresh=.01){
+
   
-  ##there is apart of the code when you use datamat, but you never
-  ##define it as a local variable. you have defined datamat as a global
-  ##variable in your script...it is what was passed into this function
-  ##as the parameter m
-  datamat <- m
-  
+  ##########################################################
+  ##                                                      ##
+  ##  Create two matrices mFilt and mVBPFilt.             ##
+  ##  mFilt has rows for VBPs and columns for VBPs+VPOs.  ##
+  ##  mVBPFilt is square with rows and columns for VBPs.  ##
+  ##  Systemetic baits removed if indicated (default).    ##
+  ##                                                      ##
+  ##########################################################
 
   stopifnot(all(m %in% 0:1))
   
-  mOrig <- m
-  allBaits <- rownames(mOrig)
-  allPrey <- setdiff(colnames(mOrig),allBaits)
+  allBaits <- rownames(m)
+  allPrey <- setdiff(colnames(m),allBaits)
   
-  ##get VBP graph
   VBPs <- allBaits[(rowSums(m)>0) & 
                    (colSums(m[,allBaits])>0)]
   VPOs <- allPrey[(colSums(m[,allPrey])>0)]
-  
-  m <- m[VBPs,c(VBPs,VPOs)]
+  mVBP <- m[VBPs,c(VBPs,VPOs)]
 
-  ##filter nodes subject to systematic error
+  
+  ## identify baits subject to systematic error
+  
+
   if(filterSystematic){
-    mb <- m[VBPs,VBPs]
+    mb <- mVBP[VBPs,VBPs]
     mbp <- assessSymmetry(mb)$p
-    lose <- which(mbp<SystematicpThresh)
-    
+    lose <- which(mbp<SystematicpThresh)    
     goodBaits <- setdiff(VBPs,names(lose))
-  }
-  else goodBaits <- VBPs
+  }  else goodBaits <- VBPs
   
   
   ##remove baits prone to systematic error
-  ##mFilt is the matrix with no systematic proteins but has VP
-  ##m now has no systematic proteins nor any only VPs
-  mFilt <- mOrig[goodBaits,c(goodBaits,VPOs)]
-  m <- mFilt[,goodBaits]
 
-  ##now, we look at the gold standard complexes and keep only those
-  ##complexes for which a large proportion of the constitute members
-  ##are found to be either non-systematic proteins or VPs...NB we
-  ##don't use VBs for the filtering criteria
-  keep1 <- apply(GS,MARGIN=2,FUN=obsProp,y=colnames(mFilt))
-  nEligComplexes <- sum(keep1)
+
+  mFilt <- m[goodBaits,c(goodBaits,VPOs)]
+  mVBPFilt <- mFilt[,goodBaits]
+
   
-  
+  ##########################################################
+  ##                                                      ##
+  ##  Now, look at the gold standard complexes.           ##
+  ##  Keep only those for which the proportion specified  ## 
+  ##  by obsPropThresh of the constituent members         ##
+  ##  are found to be either non-systematic VBPs or VPOs. ##
+  ##  NB we don't use VBs for the filtering criteria.     ##
+  ##  Put results in GSviable and incidence matrix mGS.   ##                    
+  ##                                                      ##
+  ##########################################################
+
+  keep1 <- apply(GS,MARGIN=2,FUN=obsProp,y=colnames(mFilt),obsPropThresh=obsPropThresh)
+
+
+  ##make sure edges are stochastically distributed
+  ##within a complex, e.g. we don't want all missing edges
+  ##pointing at just one protein  
+
+
   keep <- rep(FALSE,length(keep1))
   for (i in 1:length(keep1)){
     if(keep1[i]){
       prots <- names(which(GS[,i]==1))
-      protsub <- intersect(prots,c(goodBaits,VPOs))
-      baitsub <- intersect(prots,goodBaits)
-      preysub <- setdiff(protsub,baitsub)
-      ##datamat should be passed into the function as well...i don't know
-      ##where datamat came from.
-      ##TC says (a day later)...it seems that datamat is the same as mOrig
-      tempMat <- datamat[baitsub,c(baitsub,preysub),drop=FALSE]
-		diag(tempMat) <- 0
-      
-      ##don't know what is going on here in this if statement
-      if(dim(tempMat)[1]>1 & dim(tempMat)[2]<15){
-        a1 <- rowSums(tempMat)
-        b1 <- rowSums(1-tempMat)
-        b1[baitsub] <- b1[baitsub]-1
-        a2 <- colSums(tempMat)
-        b2 <- colSums(1-tempMat)
-        b2[baitsub] <- b2[baitsub]-1
-        p1 <- fisher.test(rbind(a1,b1),workspace=2e7)$p.value
-        p2 <- fisher.test(rbind(a2,b2),workspace=2e7)$p.value
-        keep[i] <- p1>0.01 & p2>0.01
-      } else keep[i] <- TRUE
+      keep[i] = checkEdgePattern(prots=prots,m=mFilt,pThresh=SystematicpThresh)
     }
   } 
   
-  
-  complexSizes <- colSums(GS[,keep])
-  
-  
-  mGS <- as.matrix(1*(GS[,keep] %*% t(GS[,keep]) >0))
+   
+  ##make GSviable and mGS
+    
+  GSviable = GS[,keep]
+  mGS <- as.matrix(1*(GSviable %*% t(GSviable) >0))
   diag(mGS) <- 0
 
-  complexSizes <- colSums(GS[,keep])
+
+
   
-  
-  mGS <- as.matrix(1*(GS[,keep] %*% t(GS[,keep]) >0))
-  diag(mGS) <- 0
-  
-  
-  
-  ##tony asks...are you only finding the baits that was also
-  ##in the GS...not all nodes...
-                                        #find nodes in both data sets
-  commonNodes <- intersect(rownames(m),rownames(mGS))
+  ##########################################################
+  ##                                                      ##
+  ##  Restrict attention to doubly tested edges           ##
+  ##  between VBPs.  Calculate numbers of                 ## 
+  ##  reciprocated and unreciprocated observations on     ##
+  ##  these 'true' edges.                                 ##
+  ##                                                      ##
+  ##########################################################
+
+    
+  #find VBPs in both data sets
+  commonNodes <- intersect(rownames(mVBPFilt),rownames(mGS))
   mgs <- mGS[commonNodes,commonNodes]
+  ms <- mVBPFilt[commonNodes,commonNodes]
   
-  nEligBaits <- sum(rowSums(mgs)>0)
-  nEligEdges <- sum(mgs)/2
-  nBaitsInComplexes <- colSums(GS[commonNodes,keep])
-  
-  ##tony commented out this print statement...
-                                        #print(summary(nBaitsInComplexes))
-  
-  ##tony asks...why use m and not mFilt?
-  ms <- m[commonNodes,commonNodes]
-  
-                                        #find subset of edges in ms that are present in mgs
-                                        #these are observed 'true positives'
+  #find subset of edges in ms that are present in mgs
+  #these are observed 'true positives'
   msTP <- ms * mgs
   
   msTPd <- getDegrees(msTP)
   mgsd <- getDegrees(mgs)[,"nr"]
   
-                                        #find global pTP estimate
-  
   totalTPR <- sum(msTPd[,"nr"])/2
   totalTPU <- sum(msTPd[,"ni"] + msTPd[,"no"])/2	  	      
   totalTP <- sum(mgsd)/2
   
+
+  ##########################################################
+  ##                                                      ##
+  ##  Calculate globalpTP and its standard error.         ##
+  ##                                                      ##
+  ##########################################################
+
   globalpTP <- estimatepTP(n=totalTP,r=totalTPR,
-                           u=totalTPU)[["pTPhat"]]
-  
-                                        #do bootstrap sampling
-  pTPBS <- rep(0,nBS)
-  datavec <- c(rep("r",totalTPR),rep("u",totalTPU),
-               rep("n",(totalTP-totalTPR-totalTPU)))
-  
-  for (bss in 1:nBS){
-    samp <- sample(x=datavec,size=totalTP,replace=TRUE)
-    sampR <- sum(samp=="r")
-    sampU <- sum(samp=="u")
-    pTPBS[bss] <- estimatepTP(n=totalTP,r=sampR,u=sampU)[["pTPhat"]] 
-  }
-  
-  pTPBSse <- sqrt(sum((pTPBS-mean(pTPBS))^2)/(nBS-1))
-  pTPBS95L <- quantile(pTPBS,probs=0.025)
-  pTPBS95U <- quantile(pTPBS,probs=0.975)
-  
-  
-  globalpTPse <- sqrt(globalpTP*(1-globalpTP)/(2*sum(mgsd)/2))
-  
-  
-                                        #find global pFP estimate
-  
-  degObs <- getDegrees(m)
-  
-  totalObsR <- sum(degObs[,"nr"])/2
-  
+                           u=totalTPU)  
+  globalpTPse <- sqrt(globalpTP*(1-globalpTP)/(2*totalTP))
+  pTP95U = globalpTP+1.96*globalpTPse
+  pTP95L = globalpTP-1.96*globalpTPse
+
+  pTP95CI = c(pTP95L,pTP95U)
+  names(pTP95CI) = c("95CIlb","95CIub")
+ 
+  ##########################################################
+  ##                                                      ##  
+  ##  Find global pFP estimate using the method of        ##
+  ##  moments approach.                                   ##
+  ##                                                      ##  
+  ##########################################################
+
+  degObs <- getDegrees(mVBPFilt) 
+  totalObsR <- sum(degObs[,"nr"])/2  
   totalObsU <- sum(degObs[,"ni"]+degObs[,"no"])/2
+  ntot = dim(mVBPFilt)[1]
   
-                                        #find point estimate for pFP
-  nintEst <- totalObsR/(globalpTP^2)
-                                        #print(nintEst)
-  nint <- (max(floor(nintEst)-500,100)):(min(floor(nintEst)+500,dim(m)[1]*(dim(m)[1]-1)/2))
-  MOMrange <-
-    estErrProbMethodOfMoments(nint=nint,nrec=totalObsR,nunr=totalObsU,ntot=dim(m)[1])
-  closestpTP <-
-    which.min(abs((1-globalpTP)-c(MOMrange[,3],MOMrange[,5])))
-  globalpFP <- c(MOMrange[,2],MOMrange[,4])[closestpTP]
+  #find point estimate and CI for pFP
+  globalpFP = estimatepFP(pTPest=globalpTP,totalObsR=totalObsR,totalObsU=totalObsU,ntot=ntot)
+  pFP95U =  estimatepFP(pTPest=pTP95U,totalObsR=totalObsR,totalObsU=totalObsU,ntot=ntot)
+  pFP95L =  estimatepFP(pTPest=pTP95L,totalObsR=totalObsR,totalObsU=totalObsU,ntot=ntot)
   
-                                        #find upper bound for pFP
-  
-  pTP95U <- globalpTP+1.96*globalpTPse
-  
-  nintEst <- totalObsR/(pTP95U^2)
-                                        #print(nintEst)
-  nint <- (max(floor(nintEst)-500,100)):(min(floor(nintEst)+500,dim(m)[1]*(dim(m)[1]-1)/2))
-  MOMrange <-
-    estErrProbMethodOfMoments(nint=nint,nrec=totalObsR,nunr=totalObsU,ntot=dim(m)[1])
-  closestpTP <-
-    which.min(abs((1-pTP95U)-c(MOMrange[,3],MOMrange[,5])))
-  pFP95U <- c(MOMrange[,2],MOMrange[,4])[closestpTP]
-  
-  
-                                        #find lower bound for pFP
-  
-  pTP95L <- globalpTP-1.96*globalpTPse
-  
-  nintEst <- totalObsR/(pTP95L^2)
-                                        #print(nintEst)
-  nint <- (max(floor(nintEst)-500,100)):(min(floor(nintEst)+500,dim(m)[1]*(dim(m)[1]-1)/2))
-  MOMrange <-
-    estErrProbMethodOfMoments(nint=nint,nrec=totalObsR,nunr=totalObsU,ntot=dim(m)[1])
-  closestpTP <-
-    which.min(abs((1-pTP95L)-c(MOMrange[,3],MOMrange[,5])))
-  pFP95L <- c(MOMrange[,2],MOMrange[,4])[closestpTP]
-  
+  pFP95CI = c(pFP95L,pFP95U)
+  names(pFP95CI) = c("95CIlb","95CIub")
+
+  ##########################################################
+  ##                                                      ##  
+  ##  Report summary statistics of portion of gold        ##
+  ##  standard used.                                      ##
+  ##                                                      ##  
+  ##########################################################
+
+  nEligComplexes <- dim(GSviable)[2]
+  nEligBaits <- sum(rowSums(mgs)>0)
+  nEligEdges <- sum(mgs)/2
+  nBaitsInComplexes <- colSums(GSviable[commonNodes,])
+  complexSizes <- colSums(GSviable)
+
+
+
   
   list(globalpTP=globalpTP,
        globalpTPse=globalpTPse,
        globalpFP=globalpFP,
-       pTP95L = pTP95L,
-       pTP95U = pTP95U,
-       pTPBSse = pTPBSse,
-       pTPBS95L = pTPBS95L,
-       pTPBS95U = pTPBS95U,
-       pFP95L = pFP95L,
-       pFP95U = pFP95U,
+       pTP95CI = pTP95CI,
+       pFP95CI = pFP95CI,
        nEligComplexes=nEligComplexes,
        nEligBaits=nEligBaits,
        nEligEdges=nEligEdges,
@@ -209,8 +172,22 @@ estimateCCMErrorRates <- function(m,GS,filterSystematic=TRUE,
 }
 
 
-                                        #Wolfgang and Tony's functions
 
+
+
+
+#####################################################
+## other functions used in estimateCCMEErrorRates  ##
+#####################################################
+
+#filter function to find complexes meeting tresholding criteria
+obsProp <- function(x,y, obsPropThresh=1){
+  xn <- names(which(x==1))
+  ans <-  length(intersect(xn,y))/sum(x) >= obsPropThresh
+  ans
+}
+
+#get reciprocated and unreciprocated degrees
 getDegrees = function(m){
   stopifnot(all(m %in% 0:1))
   m = m>0
@@ -218,27 +195,53 @@ getDegrees = function(m){
         ni=rowSums((!m) & t(m)))
 }
 
+
+#MLE estimate of pTP                                        
 estimatepTP <- function(n,r,u){
   
   stopifnot(n>0)
   pTPhat <- (2*r+u)/(2*n)
-  list(pTPhat=pTPhat)
+  pTPhat
 
 }
 
-estimatepFP <- function(tE,r,u,pTP){
-  
-  Nnum <- (2*r+u)^2-4*r*tE
-  Ndenom <- 4*pTP*(2*r+u)-4*r-4*tE*(pTP^2)
-  N <- Nnum/Ndenom
-  
-  print(N)
-  pFPhat <- ((2*r+u)-2*pTP*N)/(2*(tE-N))
-  list(pFPhat=pFPhat)
+
+#MOM estimate for pFP corresponding to given pTP
+estimatepFP = function(pTPest,totalObsR,totalObsU,ntot){
+	
+ nintEst = totalObsR/(pTPest^2)
+ nint <- (max(floor(nintEst)-500,100)):(min(floor(nintEst)+500,ntot*(ntot-1)/2))
+ MOMrange <-
+    estErrProbMethodOfMoments(nint=nint,nrec=totalObsR,nunr=totalObsU,ntot=ntot)
+ closestpTP <-
+    which.min(abs((1-pTPest)-c(MOMrange[,3],MOMrange[,5])))
+ pFPest = c(MOMrange[,2],MOMrange[,4])[closestpTP]	    
+ pFPest
 }
 
-obsProp <- function(x,y, obsPropThresh=.7){
-  xn <- names(which(x==1))
-  ans <-  length(intersect(xn,y))/sum(x) >= obsPropThresh
-  ans
-}
+
+
+#check edge pattern within candidate complex to ensure random 
+#distribution of missing edges
+checkEdgePattern = function(prots,m,pThresh){
+      protsub <- intersect(prots,colnames(m))
+      baitsub <- intersect(prots,rownames(m))
+      preysub <- setdiff(protsub,baitsub)
+
+      tempMat <- m[baitsub,c(baitsub,preysub),drop=FALSE]
+      diag(tempMat) <- 0
+      
+      ##check to make sure edges are stochastically distributed with a complex
+      if(dim(tempMat)[1]>1 & dim(tempMat)[2]<15){
+        a1 <- rowSums(tempMat)
+        b1 <- rowSums(1-tempMat)
+        b1[baitsub] <- b1[baitsub]-1
+        a2 <- colSums(tempMat)
+        b2 <- colSums(1-tempMat)
+        b2[baitsub] <- b2[baitsub]-1
+        p1 <- fisher.test(rbind(a1,b1),workspace=2e7)$p.value
+        p2 <- fisher.test(rbind(a2,b2),workspace=2e7)$p.value
+        ans <- p1>0.01 & p2>0.01
+      } else ans <- TRUE
+      ans
+    }
