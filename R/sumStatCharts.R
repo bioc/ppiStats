5f4c81713b4d8f5ef9076b68f18df1b358afd51f
Tony Chiang <tchiang@fhcrc.org>
2008-02-20 15:50:34 +0000

    i have started a file containing functions to generate summary statistics
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@30060 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/sumStatCharts.R
@@ -0,0 +1,19 @@
+createTables <- function(dataGraphs, total=6466, expNames){
+
+  dNames <- expNames
+  lapply(dataGraphs)
+  
+
+}
+
+viabilityCharts <- function(dataGraphs, total=6466){
+
+  
+
+}
+
+
+
+
+
+
