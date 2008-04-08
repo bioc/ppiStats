c8c9f6f7b4945036d180bc761cab208eea31e867
Tony Chiang <tchiang@fhcrc.org>
2008-04-08 06:37:37 +0000

    i have added the separateExptBySize func
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ppiStats@30909 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/separateExptBySize.R
@@ -0,0 +1,22 @@
+separateExptBySize <- function(listOfGraphs, bound1, bound2){
+
+  size <- sapply(listOfGraphs, function(x){
+    y <- degree(x);
+    if(isDirected(x))
+      {z <- sum(y$inDegree)}
+    else 
+      {z <- sum(y)};
+    return(z)})
+
+  small <- listOfGraphs[size<bound1]
+  medium <- listOfGraphs[size>=bound1 && size<bound2]
+  large <- listOfGraphs[size>=bound2]
+
+  newList <- list()
+  newList$smallScale <- small
+  newList$mediumScale <- medium
+  newList$largeScale <- large
+
+  return(newList)
+
+}
